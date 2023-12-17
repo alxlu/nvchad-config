db21f848a1f86c0907c5073fb9c5d76fc0dc98c4
Alex Lu <alxlu.git@fastmail.com>
2023-12-17 02:41:21 -0800

    open oil in file's directly by default

--- mappings.lua
+++ mappings.lua
@@ -25,8 +25,8 @@ M.general = {
     ["QQ"] = { "<cmd> quitall! <CR>", "quit all without save" },
     ["WW"] = { "<cmd> quit <CR>", "quit window without save" },
     ["gs"] = { "<cmd> G <CR>", "vim fugitive git status"},
-    ["<leader>fd"] = { "<cmd> Oil --float . <CR>", "open oil in float in current dir"},
-    ["-"] = { "<cmd> Oil . <CR>", "open oil in current dir"},
+    ["<leader>fd"] = { "<cmd> Oil --float<CR>", "open oil in float in current dir"},
+    ["-"] = { "<cmd> Oil<CR>", "open oil in current dir"},
     ["<leader>K"] = {
       function()
         vim.diagnostic.open_float { border = "rounded" }
